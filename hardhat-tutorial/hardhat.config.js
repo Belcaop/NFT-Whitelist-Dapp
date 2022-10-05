a39d563d5fb8a36e0aa4bd652dc51cff5430b12f
Belcaop <benjaminfrancis246@gmail.com>
2022-10-05 11:45:40 +0100

    Changed network to Goerli

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -2,18 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 
-const RINKEBY_URL = process.env.RINKEBY_URL;
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const GOERLI_URL = process.env.GOERLI_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "goerli",
   networks: {
-    rinkeby:{
-      url: RINKEBY_URL,
-      accounts:[RINKEBY_PRIVATE_KEY],
-      chainId: 4,
+    goerli:{
+      url: GOERLI_URL,
+      accounts:[GOERLI_PRIVATE_KEY],
+      chainId: 5,
     },
   },
 };
